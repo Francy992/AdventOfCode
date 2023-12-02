1f88f56a9fed4d91599d562e44da4e9d61e7dd4f
Francesco Anastasio <francescoanastasio@gipo.it>
2023-12-02 10:18:18 +0100

    Day two

---
+++ AdventOfCode/Days/2/DayTwo.cs
@@ -0,0 +1,112 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Days._2;
+
+public class DayTwo
+{
+    private readonly Dictionary<string, int> _default = new()
+    {
+        { "red", 12 },
+        { "green", 13 },
+        { "blue", 14 }
+    };
+    
+    public int ResolvePartOneSmallInput()
+    {
+        // read file row by row and create a list of string
+        var input = Utils.ReadAllLines(2, "SmallInput-1.txt");
+        return BodyPartOne(input);
+    }
+
+    private int BodyPartOne(string[] input)
+    {
+        var total = 0;
+        foreach (var game in input)
+        {
+            var gameNumber = GetGameNumber(game);
+            var isPossible = true;
+            foreach (var subGame in game.Split(":")[1].Split(";"))
+            {
+                var cubes = subGame.Split(",");
+                foreach (var cube in cubes)
+                {
+                    var color = cube.Trim().Split(" ")[1].Trim();
+                    var targetNumber = _default[color];
+                    var number = int.Parse(cube.Trim().Split(" ")[0].Trim());
+                    if (number <= targetNumber) continue;
+                    isPossible = false;
+                    break;
+                }
+                if(!isPossible)
+                    break;
+            }
+            
+            if(isPossible)
+                total += gameNumber;
+        }
+
+        return total;
+    }
+
+    private int GetGameNumber(string game)
+    {
+        // Example Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        return int.Parse(game.Split(":")[0].Split(" ")[1]);
+    }
+
+    public int ResolvePartOneLongInput()
+    {
+        // read file row by row and create a list of string
+        var input = Utils.ReadAllLines(2, "LongInput-1.txt");
+        return BodyPartOne(input);
+    }   
+    
+    public int ResolvePartTwoSmallInput()
+    {
+        // read file row by row and create a list of string
+        var input = Utils.ReadAllLines(2, "SmallInput-2.txt");
+        return BodyPartTwo(input);
+    }
+    
+    private int BodyPartTwo(string[] input)
+    {
+        var total = 0;
+        foreach (var game in input)
+        {
+            var dictForGame = new Dictionary<string, int>();
+            foreach (var subGame in game.Split(":")[1].Split(";"))
+            {
+                var cubes = subGame.Split(",");
+                foreach (var cube in cubes)
+                {
+                    var color = cube.Trim().Split(" ")[1].Trim();
+                    var number = int.Parse(cube.Trim().Split(" ")[0].Trim());
+                    
+                    if (dictForGame.ContainsKey(color))
+                    {
+                        dictForGame[color] = Math.Max(dictForGame[color], number);
+                    }
+                    else
+                    {
+                        dictForGame.Add(color, number);
+                    }
+                }
+            }
+            var currentMultiple = 1;
+            foreach (var (key, value) in dictForGame)
+            {
+                currentMultiple *= value;
+            }
+            total += currentMultiple;
+        }
+
+        return total;
+    }
+    
+    public int ResolvePartTwoLongInput()
+    {
+        var input = Utils.ReadAllLines(2, "LongInput-2.txt");
+        return BodyPartTwo(input);
+    }   
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AdventOfCode.Days._1;
+using AdventOfCode.Days._2;
 
 Console.WriteLine("First day of Advent of Code 2023!");
 var firstDay = new DayOne();
@@ -8,4 +9,10 @@ Console.WriteLine($"Result of day one for small input: {firstDay.ResolvePartOneS
 Console.WriteLine($"Result of day one for long input: {firstDay.ResolvePartOneLongInput()}");
 Console.WriteLine($"Result of day one for small input part 2: {firstDay.ResolvePartTwoSmallInput()}");
 Console.WriteLine($"Result of day one for long input part 2: {firstDay.ResolvePartTwoLongInput()}");
+
+var secondDay = new DayTwo();
+Console.WriteLine($"Result of day two for small input: {secondDay.ResolvePartOneSmallInput()}");
+Console.WriteLine($"Result of day two for long input: {secondDay.ResolvePartOneLongInput()}");
+Console.WriteLine($"Result of day two for small input part 2: {secondDay.ResolvePartTwoSmallInput()}");
+Console.WriteLine($"Result of day two for long input part 2: {secondDay.ResolvePartTwoLongInput()}");
 
